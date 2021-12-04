1ba052465d086e4811ab20acd364df6cca96c716
Alina Lescinskaite <lescinskaitealina@gmail.com>
2021-12-04 11:03:41 +0200

    Additional script added in file Task_8.py

--- Task_8.py
+++ Task_8.py
@@ -14,11 +14,23 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def perimeter(self):
+        return (self.width + self.height)*2
+
     def area(self):
         return self.width * self.height
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(width=side, height=side)
 
 
 if __name__ == '__main__':
 
-    rectan = Rectangle(5, 4)
-    print(rectan.area())
+    r1 = Rectangle(5, 4)
+    print(r1.perimeter())
+    print(r1.area())
+
+    s1 = Square(6)
+    print(s1.perimeter())
+    print(s1.area())
